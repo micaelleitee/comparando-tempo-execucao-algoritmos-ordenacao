5e8db8d44e4da6f6dd8241a7fcb1cfb71d9f4b61
Micael <micaelleite01@gmail.com>
2024-11-26 13:13:57 -0300

    inicio versão

---
+++ mergesort.py
@@ -0,0 +1,22 @@
+def mergesort (lista, inicio, fim):
+  if  inicio < fim - 1:
+    meio = (fim + inicio)//2
+    mergesort(lista, inicio, meio)
+    mergesort(lista, meio, fim)
+    merge(lista, inicio, meio, fim)
+
+def merge(lista,inicio, meio, fim):
+  esquerda = lista[inicio:meio] + [float('inf')]
+  direita = lista[meio:fim] + [float('inf')]
+  topo_esquerda, topo_direita = 0, 0
+  for i in range (inicio, fim):    
+    if esquerda[topo_esquerda] < direita[topo_direita]:
+      lista[i] = esquerda[topo_esquerda]
+      topo_esquerda = topo_esquerda + 1
+    else:
+      lista[i] = direita[topo_direita]
+      topo_direita = topo_direita + 1
+
+lista = [38, 27, 43, 3, 9, 82, 10]
+mergesort(lista, 0, len(lista))
+print(lista)
