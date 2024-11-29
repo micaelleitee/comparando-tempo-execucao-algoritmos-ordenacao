a430aaa92cab35ad86900b94c06aa8cadc10bebf
Micael <micaelleite01@gmail.com>
2024-11-28 22:49:09 -0300

    mudando o codigo do quicksort

--- quicksort.py
+++ quicksort.py
@@ -9,8 +9,12 @@ def partition(A,p,r):
   i = p -1
   for j in range (p, r):
     if A[j] <= x:
-      i = i +1
-      A[i] = A[j]
-  A[i+1] = A[r]
+      i +=1
+      A[i], A[j] = A[j], A[i]
+  A[i+1], A[r] = A[r], A[i+1]
   return i+1
 
+lista =[10,55,90,30,60]
+n = len(lista)
+quicksort(lista,0, n-1)
+print(lista)
